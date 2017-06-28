7eac434c170f501a52fd6d30d7f6183dc4124d67
knt9mac <matsuyamak@icloud.com>
2017-06-29 01:01:11 +0900

    mongodb接続設定

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -16,7 +16,7 @@ http.listen(project.server_port)
 debug(`Server is now running at http://localhost:${project.server_port}.`)
 
 // データベースを接続
-mongoose.connect('mongodb://minakawa:wedding0722@ds141082.mlab.com:41082/nullpon/')
+mongoose.connect('mongodb://minakawa:wedding0722@ds141082.mlab.com:41082/heroku_3g8w655/')
 mongoose.Promise = global.Promise
 
 io.on('connection', function (socket) {
