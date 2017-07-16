fabecfd5adce242f8367df09fa10ee9a1ef7544e
knt9mac <matsuyamak@icloud.com>
2017-07-16 21:59:39 +0900

    ソート処理修正

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -133,7 +133,7 @@ io.on('connection', function (socket) {
     debugSocket('resultAnnouncement')
 
     // 各ユーザーの正解数
-    Users.find({}, {sort:{correct_answer_count: -1}}, function(err, docs) {
+    Users.find({}, {}, {sort:{correct_answer_count: -1}}, function(err, docs) {
       let resultlist = []
       for (let i = 0, size = docs.length; i < size; ++i) {
         if (docs[i]) {
@@ -144,8 +144,6 @@ io.on('connection', function (socket) {
           })
         }
       }
-      console.log(resultlist)
-      fn(resultlist)
     })
   })
 })
