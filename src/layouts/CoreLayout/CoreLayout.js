1087fdd63aa09a93aa2f48ef1587dfb0efc4208b
sh <02luvsic@gmail.com>
2017-07-18 20:41:05 +0900

    通信状態を通知するsnackbarの作成

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Snackbar from 'material-ui/Snackbar';
+
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
@@ -14,6 +16,30 @@ class CoreLayout extends React.Component {
     muiTheme: PropTypes.object.isRequired
   }
 
+  state = {
+    open            : false,
+    message         : false,
+    isSocketConnect : false
+  }
+
+  componentDidMount () {  
+    window.socket.on('connect', () => {
+      this.setSocketStatus('connect')
+      this.setMessage('接続しました!')
+      this.showSnack()
+
+      setTimeout(() => {
+        this.hideSnack()
+      }, 4000)
+    })
+
+    window.socket.on('disconnect', () => {
+      this.setSocketStatus('disconnect')
+      this.setMessage('接続が切れました...')
+      this.showSnack()
+    })
+  }
+
   getChildContext () {
     return {
       muiTheme: getMuiTheme({
@@ -22,14 +48,68 @@ class CoreLayout extends React.Component {
     }
   }
 
+  setMessage = (message) => {
+    this.setState({
+      message
+    })
+  }
+  setSocketStatus = (status) => {
+    if(status === 'connect') {
+      this.setState({
+        isSocketConnect : true
+      })
+    } else if(status === 'disconnect') {
+      this.setState({
+        isSocketConnect : false
+      })
+    }
+  }
+
+  showSnack = () => {
+    this.setState({
+      open: true,
+    })
+  }
+  hideSnack = () => {
+    this.setState({
+      open: false,
+    });
+  }
+
+  reconnect = () => {
+    try {
+      socket.open()
+    } catch (e) {
+      console.warn(e)
+    }
+  }
+
   render () {
-    const {children} =  this.props
+    const { children } = this.props
+    const { message, open, isSocketConnect } = this.state
 
     return (
       <div>
         <div className='core-layout__viewport'>
           {children}
         </div>
+        <Snackbar
+          open={ open }
+          message={ message }
+          action={ !isSocketConnect ? '再接続する' : '' }
+          onActionTouchTap={ !isSocketConnect ? this.reconnect : ''}
+          onRequestClose={(e) => console.log(e)}
+
+          contentStyle={{
+            textAlign: 'center'
+          }}
+
+          bodyStyle={{
+            height: 'auto'
+          }}
+
+          className='snackbar'
+        />
       </div>
     )
   }
